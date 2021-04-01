246d87b0f4e7a5f8d6b5a908f24aa7b8876f8a72
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-04-01 23:40:36 +0200

    Polish & add timer

--- src/main/java/com/azortis/azorbot/commands/wiki/wikiCreate.java
+++ src/main/java/com/azortis/azorbot/commands/wiki/wikiCreate.java
@@ -3,6 +3,7 @@ package com.azortis.azorbot.commands.wiki;
 import com.azortis.azorbot.Main;
 import com.azortis.azorbot.util.AzorbotCommand;
 import com.azortis.azorbot.util.AzorbotEmbed;
+import com.azortis.azorbot.util.ExecutionTimer;
 import com.azortis.azorbot.util.WikiIndexed;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -25,21 +26,24 @@ public class wikiCreate extends AzorbotCommand {
     @Override
     public void handle(List<String> args, GuildMessageReceivedEvent e){
         AzorbotEmbed embed = new AzorbotEmbed("Wiki creation", e.getMessage());
-        if (args.size() == 1) {
+        if (args.size() == 1){
             embed.setDescription("You forgot to specify the raw GitPath and Docs page");
             sendHelp(e.getMessage());
-        } else if (args.size() == 2) {
+        } else if (args.size() == 2){
             embed.setDescription("You forgot to specify the Docs page");
             sendHelp(e.getMessage());
-        } else if (args.get(0).length() < 4) {
+        } else if (args.get(0).length() < 4){
             embed.setDescription("The specified wiki name is too short (min 4 characters)");
-        } else if (!args.get(2).contains("https://raw.githubusercontent.com/")) {
+        } else if (!args.get(2).contains("https://raw.githubusercontent.com/")){
             embed.setDescription("Your GitPath does not contain the required main path:\n`" + args.get(1) + "`");
             embed.addField("GitPath Help", "You can request GitPath help with `" + Main.prefix + "wiki GitPath`", false);
         } else {
-            embed.setDescription("Created new wiki " + args.get(0));
-            embed.addField("Loaded wikis: ", WikiIndexed.getLoadedWikis(), false);
+            ExecutionTimer timer = new ExecutionTimer();
             new WikiIndexed(args.get(0), args.get(2), args.get(1));
+            timer.stop();
+            embed.setDescription("Created new wiki " + args.get(0) + "\n" +
+                    timer.duration("Create Wiki"));
+            embed.addField("Loaded wikis: ", WikiIndexed.getLoadedWikis(), false);
         }
         embed.send(true, 60000);
     }
