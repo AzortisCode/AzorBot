26ae96f7bfe14140d45960629acfb040b414c1b9
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-04-05 20:44:18 +0200

    Revert "Revert "Set default path for config to "config/"""
    This reverts commit 295ef9d0e77e0e82db436d7f1e109a912db5383d.

--- src/main/java/com/azortis/azorbot/cocoUtil/CocoTimer.java
+++ src/main/java/com/azortis/azorbot/cocoUtil/CocoTimer.java
@@ -1,11 +1,21 @@
 package com.azortis.azorbot.cocoUtil;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class CocoTimer {
+    private static final List<CocoTimer> timers = new ArrayList<>();
     private long start;
     private long end;
+    @Getter
+    private long ID;
 
     /**
      * Creates a new execution timer that immediately starts running
+     * <p>This is not added to the list of timers stored</p>
      */
     public CocoTimer(){
         reset();
@@ -13,10 +23,14 @@ public class CocoTimer {
     }
 
     /**
-     * Stops the execution timer
+     * Creates a new execution timer that immediately starts running
+     * @param ID The ID used for querying timers
      */
-    public void end(){
-        end = System.currentTimeMillis();
+    public CocoTimer(long ID){
+        reset();
+        start = System.currentTimeMillis();
+        this.ID = ID;
+        timers.add(this);
     }
 
     /**
@@ -27,11 +41,28 @@ public class CocoTimer {
     }
 
     /**
+     * Stops the execution timer
+     * @param ID ID to query for timer
+     */
+    public void stop(long ID){
+        Objects.requireNonNull(findTimer(ID)).stop();
+    }
+
+    /**
      * Gets the duration
      * @return duration in MS
      */
     public long durationLong(){
         return (end - start);
+    }
+
+    /**
+     * Gets the duration
+     * @param ID ID to query for timer
+     * @return duration in MS
+     */
+    public static long durationLong(long ID){
+        return Objects.requireNonNull(findTimer(ID)).durationLong();
     }
 
     /**
@@ -46,6 +77,16 @@ public class CocoTimer {
     /**
      * Build duration string with command name
      * <p>Running `command` took `###`ms</p>
+     * @param ID ID to query for timer
+     * @return String with duration in format
+     */
+    public static String duration(long ID){
+        return Objects.requireNonNull(findTimer(ID)).duration();
+    }
+
+    /**
+     * Build duration string with command name
+     * <p>Running `command` took `###`ms</p>
      * @param command Name of the command used in format
      * @return String with duration in format
      */
@@ -54,10 +95,41 @@ public class CocoTimer {
     }
 
     /**
+     * Build duration string with command name
+     * <p>Running `command` took `###`ms</p>
+     * @param command Name of the command used in format
+     * @param ID ID to query for timer
+     * @return String with duration in format
+     */
+    public static String duration(String command, long ID){
+        return Objects.requireNonNull(findTimer(ID)).duration(command);
+    }
+
+    /**
      * Resets the timer
      */
-    public void reset(){
+    private void reset(){
         start = 0;
         end = 0;
+    }
+
+    /**
+     * Deletes a timer
+     * @param ID ID to query for timer
+     */
+    public static void deleteTimer(long ID){
+        timers.remove(findTimer(ID));
+    }
+
+    /**
+     * Finds a timer by ID
+     * @param ID ID to query for timer
+     * @return found timer (null if none found)
+     */
+    public static CocoTimer findTimer(long ID){
+        for (CocoTimer timer : timers) {
+            if (timer.getID() == ID) return timer;
+        }
+        return null;
     }
 }
