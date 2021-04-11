86c3ea391884968499b8508e2db60ae4cc1b62d1
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-04-11 14:10:49 +0200

    truncate stupidly long commands

--- src/main/java/com/azortis/azorbot/listeners/ChatListener.java
+++ src/main/java/com/azortis/azorbot/listeners/ChatListener.java
@@ -9,7 +9,7 @@ public class ChatListener extends ListenerAdapter {
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent e){
         if (!e.getAuthor().isBot()){
-            CocoBot.info(e.getAuthor().getName() + ": " + e.getMessage().getContentDisplay());
+            CocoBot.info(e.getAuthor().getName() + ": " + e.getMessage().getContentDisplay().substring(0, 40) + (e.getMessage().getContentDisplay().length()>=40 ? " (...)" : ""));
         }
     }
 }
